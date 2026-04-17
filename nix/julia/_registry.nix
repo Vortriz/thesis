eacb45606443230f44978db4d5a1b55deabaa0cc
Vortriz <vorarishi22+github@gmail.com>
2026-04-17 22:41:05 +0530

    chore: update deps

--- nix/julia/_registry.nix
+++ nix/julia/_registry.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "codedownio";
   repo = "General";
-  rev = "832481456a216c560d4334ec56c069955b23aa7f";
-  hash = "sha256-UiEd37FRAYvJ/thZAnPus4m2rFGfRL24DpIbFNieesc=";
+  rev = "cf28aaea90d7aa9e481ae7f9a50cc1f0311cde1f";
+  hash = "sha256-3XUcGy06f2W/wWcjxpg0wdpM6PQJc0XgewrjmVguNlg=";
 }
